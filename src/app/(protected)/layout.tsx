94e3021460d3c5f8551acb846cd86345f41e9cd8
Shashwot90 <shashwotpradhan@gmail.com>
2025-08-21 06:21:56 +0545

    typ

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -2,3 +2,4 @@ import { SidebarProvider } from '@/components/ui/sidebar'
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 import { AppSidebar } from './dashboard/app-sidebar'
+type Props = {
