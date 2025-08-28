20c163b4147c79988c8a67619b57b3b45dbc8e36
Shashwot90 <shashwotpradhan@gmail.com>
2025-08-28 06:15:31 +0545

    div

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -16,3 +16,4 @@ const SidebarLayout = ({children}: Props) => {
                 <div className="ml-auto"></div>
                 <UserButton />
             </div>
+            <div className="h-4"></div>
