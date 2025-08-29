3b68f23a98feecd4eddd147093e7b348cd6d9d0d
Shashwot90 <shashwotpradhan@gmail.com>
2025-08-29 06:10:05 +0545

    child

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -20,10 +20,3 @@ const SidebarLayout = ({children}: Props) => {
             {/* main content */}
             <div className="border-sidebar-border bg-sidebar border shadow rounded-md overflow-y-scroll h-[calc(100vh-6rem)] p-4">
                 {children}
-            </div>
-        </main>
-    </SidebarProvider>
-  )
-}
-
-export default SidebarLayout
