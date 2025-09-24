e42a142a05303978901c3ec908541d80d4a9aacb
Shashwot90 <shashwotpradhan@gmail.com>
2025-09-24 06:23:35 +0545

    export

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,2 +1,13 @@
 import { Button } from "@/components/ui/button";
 
+
+export default async function Home() {
+     
+  return (
+    <>
+    
+    <h1 className="text-red-600">Hello</h1>
+    <Button>Click</Button>
+    </>
+  );
+}
