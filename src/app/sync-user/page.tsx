bf541231ba74b62f53bb1bd31225a6a8f0e44159
Shashwot90 <shashwotpradhan@gmail.com>
2025-10-06 06:42:51 +0545

    await

--- src/app/sync-user/page.tsx
+++ src/app/sync-user/page.tsx
@@ -8,3 +8,29 @@ const SyncUser =  async () => {
     if (!userId) {
         throw new Error('User not found')
     }
+    const client = await clerkClient()
+    const user = await client.users.getUser(userId)
+    if (!user.emailAddresses[0]?.emailAddress) {
+        return notFound()
+    }
+    await db.user.upsert({
+        where: {
+            emailAddress: user.emailAddresses[0]?.emailAddress ?? ""
+        },
+        update: {
+            imageUrl: user.imageUrl,
+            firstName: user.firstName,
+            lastName: user.lastName, 
+        },
+        create: {
+            id: userId,
+            emailAddress: user.emailAddresses[0]?.emailAddress ?? "",
+            imageUrl: user.imageUrl,
+            firstName: user.firstName,
+            lastName: user.lastName,
+        }
+    })
+  return  redirect('/dashboard')
+}
+
+export default SyncUser
