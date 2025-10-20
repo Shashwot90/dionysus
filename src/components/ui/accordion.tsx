8feb89a5c1157de96217e565da776712385346bf
Shashwot90 <shashwotpradhan@gmail.com>
2025-10-20 06:45:01 +0545

    classname

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -11,4 +11,6 @@ const Accordion = AccordionPrimitive.Root
 
 const AccordionItem = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
+>(({ className, ...props }, ref) => (
   
