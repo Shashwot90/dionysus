652ac17da8fa4bc29b3dfad021a5a8f3799e66f7
Shashwot90 <shashwotpradhan@gmail.com>
2025-10-30 06:24:46 +0545

    class

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -48,11 +48,3 @@ const AccordionContent = React.forwardRef<
   <AccordionPrimitive.Content
     ref={ref}
     className="overflow-hidden text-sm data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
-    {...props}
-  >
-    <div className={cn("pb-4 pt-0", className)}>{children}</div>
-  </AccordionPrimitive.Content>
-))
-AccordionContent.displayName = AccordionPrimitive.Content.displayName
-
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
