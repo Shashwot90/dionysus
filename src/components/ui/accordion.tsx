da92ba1ed8e2e7b04ffe25eac2b20816e4d9c96d
Shashwot90 <shashwotpradhan@gmail.com>
2025-11-01 06:37:22 +0545

    classname

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -50,3 +50,4 @@ const AccordionContent = React.forwardRef<
     className="overflow-hidden text-sm data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
     {...props}
   >
+    <div className={cn("pb-4 pt-0", className)}>{children}</div>
