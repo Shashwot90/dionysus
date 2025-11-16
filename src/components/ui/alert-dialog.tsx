84b501be568f4fdd4a8e50543587675cdc5104e3
Shashwot90 <shashwotpradhan@gmail.com>
2025-11-16 06:37:43 +0545

    react comp

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -29,3 +29,4 @@ AlertDialogOverlay.displayName = AlertDialogPrimitive.Overlay.displayName
 
 const AlertDialogContent = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Content>
