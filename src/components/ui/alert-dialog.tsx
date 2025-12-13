7f50544204e7011a3208b7630e26c443e474fe96
Shashwot90 <shashwotpradhan@gmail.com>
2025-12-13 06:34:06 +0545

    alert dialog

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -86,3 +86,5 @@ const AlertDialogTitle = React.forwardRef<
 AlertDialogTitle.displayName = AlertDialogPrimitive.Title.displayName
 
 const AlertDialogDescription = React.forwardRef<
+  React.ElementRef<typeof AlertDialogPrimitive.Description>,
+ 
