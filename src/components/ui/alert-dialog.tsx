a45a74278f8b31ddb8553a3f12b457c292a27aa9
Shashwot90 <shashwotpradhan@gmail.com>
2025-12-15 06:46:44 +0545

    classnamme

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -91,4 +91,5 @@ const AlertDialogDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AlertDialogPrimitive.Description
     ref={ref}
-   
+    className={cn("text-sm text-muted-foreground", className)}
+
