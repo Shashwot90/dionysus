7735636e5cd1aca762183c61b60ebc33b71ff1c2
Shashwot90 <shashwotpradhan@gmail.com>
2025-12-20 07:17:08 +0545

    alert dialog

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -102,3 +102,4 @@ const AlertDialogAction = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Action>,
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Action>
 >(({ className, ...props }, ref) => (
+  <AlertDialogPrimitive.Action
