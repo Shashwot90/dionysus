5f328ee0e5f57eab56e9d2ae6051c2aa5419032d
Shashwot90 <shashwotpradhan@gmail.com>
2026-01-11 06:55:09 +0545

    bor

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -10,3 +10,4 @@ const alertVariants = cva(
       variant: {
         default: "bg-background text-foreground",
         destructive:
+          "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
