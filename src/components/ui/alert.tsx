9c4030920e634823c9ef1effe88217c14d522325
Shashwot90 <shashwotpradhan@gmail.com>
2026-01-16 06:33:58 +0545

    close

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -22,3 +22,4 @@ const alertVariants = cva(
 const Alert = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
+>(({ className, variant, ...props }, ref) => (
