a9366bb02996957d1a3dd0dd44f21d56f37c239d
Shashwot90 <shashwotpradhan@gmail.com>
2026-02-24 06:06:50 +0545

    border

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -12,4 +12,5 @@ const badgeVariants = cva(
 
           "border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80",
         secondary:
+          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
         
