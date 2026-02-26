8d9c3d4458cf8e41ef1f9e8e8ef51a3eddc14b34
Shashwot90 <shashwotpradhan@gmail.com>
2026-02-26 06:56:46 +0545

    close

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -16,4 +16,5 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80",
         outline: "text-foreground",
-      
+      },
+    
