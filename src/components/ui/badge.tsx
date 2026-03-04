9ed1fa6ba76aa1332852d5998095bde311b0327e
Shashwot90 <shashwotpradhan@gmail.com>
2026-03-04 06:37:50 +0545

    div

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -30,3 +30,4 @@ export interface BadgeProps
 
 function Badge({ className, variant, ...props }: BadgeProps) {
   return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
