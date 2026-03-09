9e888fbb9baf9e627449faa8aab73213508d51ea
Shashwot Pradhan <shashwotpradhan@gmail.com>
2026-03-09 06:41:56 -0700

    prop

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -9,3 +9,4 @@ const Breadcrumb = React.forwardRef<
   React.ComponentPropsWithoutRef<"nav"> & {
     separator?: React.ReactNode
   }
+>(({ ...props }, ref) => <nav ref={ref} aria-label="breadcrumb" {...props} />)
