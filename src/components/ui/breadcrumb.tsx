b0ba077e0a9bffc42a4364905acecec3f79af8ad
Shashwot Pradhan <shashwotpradhan@gmail.com>
2026-03-12 06:45:40 +0545

    prop

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -22,4 +22,4 @@ const BreadcrumbList = React.forwardRef<
       "flex flex-wrap items-center gap-1.5 break-words text-sm text-muted-foreground sm:gap-2.5",
       className
     )}
-    
+    {...props}
