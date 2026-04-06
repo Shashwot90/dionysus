c7b766f00fcd0b8f268ed8f87ce923e62f09eed5
Shashwot Pradhan <shashwotpradhan@gmail.com>
2026-04-06 06:07:27 +0545

    props

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -82,4 +82,5 @@ const BreadcrumbPage = React.forwardRef<
         role="presentation"
         aria-hidden="true"
         className={cn("[&>svg]:w-3.5 [&>svg]:h-3.5", className)}
-        
+        {...props}
+      >
