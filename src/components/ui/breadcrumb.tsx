e8b863480c004f4d7f708dc135184596f7c925fa
Shashwot Pradhan <shashwotpradhan@gmail.com>
2026-04-14 06:31:13 +0545

    more

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -99,3 +99,4 @@ const BreadcrumbPage = React.forwardRef<
         className={cn("flex h-9 w-9 items-center justify-center", className)}
         {...props}
       >
+        <MoreHorizontal className="h-4 w-4" />
