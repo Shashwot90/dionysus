7e5d37bff0ae2b712e3e88c7074de49c1d800f67
Shashwot Pradhan <shashwotpradhan@gmail.com>
2026-05-01 05:45:26 +0545

    size

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,3 +20,5 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
+      size: {
+       
