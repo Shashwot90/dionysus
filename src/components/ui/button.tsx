6251840f65647dca9f4c744799aab5a388094bc4
Shashwot Pradhan <shashwotpradhan@gmail.com>
2026-05-14 06:21:40 +0545

    close

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -49,3 +49,10 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
+      />
+    )
+  }
+)
+Button.displayName = "Button"
+
+export { Button, buttonVariants }
