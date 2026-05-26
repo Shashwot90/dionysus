7e62106bbed1af8a8d48fb96752a5ecc91944d05
Shashwot Pradhan <shashwotpradhan@gmail.com>
2026-05-27 05:36:27 +0545

    nav

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -25,4 +25,4 @@ function Calendar({
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium",
-        
+        nav: "space-x-1 flex items-center",
