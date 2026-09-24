ddf739439ac453b91eb3c9412aab5213e4ecd0ae
Shashwot Pradhan <shashwotpradhan@gmail.com>
2026-09-24 06:36:42 +0545

    div

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -136,3 +136,4 @@ const Carousel = React.forwardRef<
                                                                 }}
                                                                 >
                                                                           <div
+        ref={ref}
